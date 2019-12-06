194c4ea706f40978d40c40f70f0d5cf078c30c33
DRTAG <masan.korea@gmail.com>
2019-12-06 15:32:21 +0900

    disable
    disabled 부분과 jquery 추가

--- popup.js
+++ popup.js
@@ -1,8 +1,18 @@
-let btn1=document.querySelector("#scrollDown");
-let btn2=document.querySelector("#chooseFile");
-let btn3=document.querySelector("#myscript1");
-let btn4=document.querySelector("#myscript2");
-
+const btn2=document.querySelector("#chooseFile");
+const btn3=document.querySelector("#myscript1");
+const btn4=document.querySelector("#myscript2");
+const btn1=document.querySelector("#scrollDown");
+const chkopt1=document.querySelector("#chkopt1");
+const chkopt2=document.querySelector("#chkopt2");
+const chkopt3=document.querySelector("#chkopt3");
+const chkopt4=document.querySelector("#chkopt4");
+const btn_auto=document.querySelector("#btn_auto");
+const auto_msg=$("#auto_msg");
+const n_auto=$("#n_auto");
+//
+btn_auto.disabled=true;
+n_auto.disabled=true;
+//
 btn1.addEventListener('click',function(e){
     chrome.tabs.query({active:true,currentWindow:true},function(tabs){
         chrome.tabs.executeScript({file:'js/scrolldown.js'})
@@ -45,4 +55,27 @@ btn4.addEventListener('click',function(e){
         });
     });
     window.close();
+});
+const chkauto=(obj)=> {
+    let j=obj.checked||chkopt2.checked||chkopt3.checked||chkopt4.checked;
+    if(j) {
+        btn_auto.disabled=false;
+        n_auto.disabled=false;
+    } else {
+        btn_auto.disabled=true;
+        n_auto.disabled=true;
+    }
+}
+chkopt1.addEventListener('click',function(e){
+    chkauto(this);
+    
+});
+chkopt2.addEventListener('click',function(e){
+    chkauto(this);
+});
+chkopt3.addEventListener('click',function(e){
+    chkauto(this);
+});
+chkopt4.addEventListener('click',function(e){
+    chkauto(this);
 });
